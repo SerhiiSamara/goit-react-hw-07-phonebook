c2434f34af955bb8ce7d3cd1b6357577799b92b2
SerhiiSamara <ndaair@gmail.com>
2023-01-06 13:42:31 +0200

    3

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -34,26 +34,7 @@ const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
-
-    // addContact: {
-    //   reducer(state, action) {
-    //     state.push(action.payload);
-    //   },
-    //   prepare(name, number) {
-    //     return {
-    //       payload: {
-    //         name,
-    //         number,
-    //       },
-    //     };
-    //   },
-    // },
-    // deleteContact(state, action) {
-    //   const index = state.findIndex(contact => contact.id === action.payload);
-    //   state.splice(index, 1);
-    // },
   },
 });
 
-// export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
